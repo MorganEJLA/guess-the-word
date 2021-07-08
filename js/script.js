7da041188053869c4d7e2a4df1dca72f39360899
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2021-07-08 16:36:56 -0400

    added input validators event handlers for the guess button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const textBox = document.querySelector(".letter");
 const progressParagraph = document.querySelector(".word-in-progress");
@@ -8,8 +8,8 @@ const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again hide");
 
 
-const word = "magnolia"
-
+const word = "magnolia";
+const guessedLetters = [];
 
 
 /* Write a function to Add Placeholders for Each Letter */
@@ -33,6 +33,44 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e){
     e.preventDefault();
+
     const guess = textBox.value;
-    console.log(guess);
+    // console.log(textBox.value);
+    message.innerText = "";
+
+  
+    const goodGuess = playerInput(guess);
+
+    if (goodGuess){
+        makeGuess(guess);
+    }
+    textBox.value = "";
+    console.log(goodGuess);
+    
 });
+
+const playerInput = function (input){
+
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length ===0){
+        message.innerText = "Please enter a letter"
+    }else if(input.length>1){
+        message.innerText = "please enter one letter only"
+    }else if (!input.match(acceptedLetter)){
+        message.innerText =  "Please enter a valid letter A-Z"
+    } else {
+        return input;
+    }
+    
+};
+
+const makeGuess = function (guess){
+    guess = guess.toUpperCase();
+    if(guessedLetters.includes(guess)){
+        message.innerText =  "You have already guessed that letter.Try again";
+    } else {
+
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
